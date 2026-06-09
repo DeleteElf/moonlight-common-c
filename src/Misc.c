85d81e51fe3232fa3df8d40a619ab10e89666053
chenhao <48207475@qq.com>
2026-06-09 15:36:38 +0800

    1.增加获取毫秒的方法支持。

--- src/Misc.c
+++ src/Misc.c
@@ -67,6 +67,10 @@ uint64_t LiGetMillis(void) {
     return PltGetMillis();
 }
 
+uint64_t LiGetMicroseconds(void) {
+    return PltGetMicroseconds();
+}
+
 uint32_t LiGetHostFeatureFlags(void) {
     return SunshineFeatureFlags;
 }
